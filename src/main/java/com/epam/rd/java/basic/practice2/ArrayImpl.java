4b8c6900a815486f99dff155da1e3ddfedb7255b
iyhunko <iyhunko@gmail.com>
2020-12-05 18:16:17 +0200

    Fix ArrayImpl methods

--- src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
@@ -7,35 +7,29 @@ public class ArrayImpl implements Array {
 
     private Object[] items;
 
-    private final int arrayLength;
+    private int arrayLength;
 
     public ArrayImpl(int size) {
-        arrayLength = size;
         items = new Object[size];
     }
 
-	@Override
+    @Override
     public void clear() {
+        arrayLength = 0;
         items = new Object[arrayLength];
     }
 
-	@Override
+    @Override
     public int size() {
-        int count = 0;
-
-        for (Object item : items)
-            if (item != null)
-                count++;
-
-	    return count;
+        return arrayLength;
     }
-	
-	@Override
+
+    @Override
     public Iterator<Object> iterator() {
-	    return new IteratorImpl();
+        return new IteratorImpl();
     }
-	
-	private class IteratorImpl implements Iterator<Object> {
+
+    private class IteratorImpl implements Iterator<Object> {
 
         private int nextIndex = 0;
 
@@ -61,35 +55,66 @@ public class ArrayImpl implements Array {
         }
 
     }
-	
-	@Override
+
+    @Override
     public void add(Object element) {
-        items[size()] = element;
+        set(arrayLength, element);
     }
 
-	@Override
+    @Override
     public void set(int index, Object element) {
+        if (index > items.length - 1) {
+            Object[] newArray = new Object[index];
+            for (int i = 0; i < items.length; i++) {
+                newArray[i] = items[i];
+            }
+            newArray[index] = element;
+
+            items = newArray;
+        }
         items[index] = element;
+        arrayLength++;
     }
 
-	@Override
+    @Override
     public Object get(int index) {
-	    return items[index];
+        if (index > items.length - 1) {
+            throw new NoSuchElementException();
+        }
+
+        return items[index];
     }
 
-	@Override
+    @Override
     public int indexOf(Object element) {
         for (int i = 0; i < items.length; i++)
             if (element.equals(items[i]))
                 return i;
 
         throw new NoSuchElementException();
-
     }
 
-	@Override
+    @Override
     public void remove(int index) {
-        items[index] = null;
+        if (index > items.length - 1) {
+            throw new NoSuchElementException();
+        }
+
+        Object[] newArray = new Object[items.length - 1];
+
+        for (int i = 0; i < newArray.length; i++) {
+            if (i > index) {
+                newArray[i] = items[i+1];
+            } else if (i < index) {
+                newArray[i] = items[i];
+            } else {
+                newArray[i] = items[i+1];
+            }
+        }
+
+        items = newArray;
+
+        arrayLength--;
     }
 
     @Override
@@ -99,7 +124,7 @@ public class ArrayImpl implements Array {
         for (int i = 0; i < items.length; i++) {
             result.append(items[i]);
 
-            if ((i+1) != items.length) {
+            if ((i + 1) != items.length) {
                 result.append(", ");
             }
         }
@@ -113,13 +138,39 @@ public class ArrayImpl implements Array {
         arrayImpl.add(5);
         arrayImpl.add(2);
 
+        System.out.println("First:");
         System.out.println(arrayImpl.get(0));
+        System.out.println("Second:");
         System.out.println(arrayImpl.get(1));
+        System.out.println("Third:");
         System.out.println(arrayImpl.get(2));
+        System.out.println("Size:");
         System.out.println(arrayImpl.size());
+        System.out.println("Index of 2:");
         System.out.println(arrayImpl.indexOf(2));
 
         System.out.println("toString:");
+        System.out.println(arrayImpl);
+
+        arrayImpl.set(2, "new 2");
+
+        System.out.println("After set 2:");
+        System.out.println(arrayImpl);
+
+        System.out.println("After set 2:");
+        System.out.println(arrayImpl);
+
+        System.out.println("After remove second element:");
+        arrayImpl.remove(1);
+        System.out.println(arrayImpl);
+
+        System.out.println("Size:");
+        System.out.println(arrayImpl.size());
+
+        arrayImpl.clear();
+        System.out.println("Size after clear:");
+        System.out.println(arrayImpl.size());
+        System.out.println("toString after clear:");
         System.out.println(arrayImpl);
     }
 
