f045b70e6fca987a59bde531d2e6212f2d24b843
iyhunko <iyhunko@gmail.com>
2020-12-12 13:56:27 +0200

    Fix test

--- src/test/java/com/epam/rd/java/basic/practice2/QueueImplTest.java
+++ src/test/java/com/epam/rd/java/basic/practice2/QueueImplTest.java
@@ -10,11 +10,11 @@ public class QueueImplTest {
     @Test
     public void shouldAddNewItemsAndReturnStringRepresentationOfQueue()
     {
-        QueueImpl queue = new QueueImpl(new String[]{"1", "2", "3"});
+        QueueImpl queue = new QueueImpl();
 
         queue.enqueue("4");
 
-        assertThat(queue.toString(), CoreMatchers.containsString("[1, 2, 3, 4"));
+        assertThat(queue.toString(), CoreMatchers.containsString("[4"));
     }
 
 }
